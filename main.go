d6e96d476ae352285c95f91555f966d4d3b446a4
Renewdxin <cissyrenxin@icloud.com>
2023-11-26 21:49:46 +0800

    start file

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package BloginGin
+package main
+
+import (
+	"BloginGin/internal/routers"
+	"net/http"
+	"time"
+)
+
+func main() {
+	router := routers.NewRouter()
+
+	// 自定义http.server
+	s := &http.Server{
+		Addr:           ":9090",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}

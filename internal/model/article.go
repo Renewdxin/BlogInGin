53618974d605c35cf96034390c2d8f4338eb43af
Renewdxin <cissyrenxin@icloud.com>
2023-11-26 21:31:40 +0800

    model define

---
+++ internal/model/article.go
@@ -0,0 +1,15 @@
+package model
+
+// define Article
+type Article struct {
+	*Model
+	Title         string `json:"title"`
+	Desc          string `json:"desc"`
+	Content       string `json:"content"`
+	CoverImageUrl string `json:"cover_image_url"`
+	State         uint8  `json:"state"`
+}
+
+func (A Article) TableName() string {
+	return "blog_article_tag"
+}

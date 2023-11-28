7b5452e81dd10ca6772478ac0163978bc7dfbb6c
Renewdxin <cissyrenxin@icloud.com>
2023-11-28 18:17:31 +0800

    项目公共组件初始化

--- pkg/errcode/common_code.go
+++ pkg/errcode/common_code.go
@@ -1,1 +1,14 @@
 package errcode
+
+var (
+	Success                    = NewError(0, "success")
+	ServerError                = NewError(-1, "server error")
+	InvalidParams              = NewError(-2, "invalid params")
+	NotFound                   = NewError(-3, "not found")
+	UnauthorizedAuthNotExist   = NewError(-4, "unauthorized, auth not exist")
+	UnauthorizedTokenError     = NewError(-5, "unauthorized, token error")
+	UnauthorizedTokenTimeout   = NewError(-6, "unauthorized, token timeout")
+	UnauthorizedTokenGenerate  = NewError(-7, "unauthorized, token generate error")
+	TooManyRequests            = NewError(-8, "too many requests")
+	UnauthorizedAuthTokenError = NewError(-9, "unauthorized, auth token error")
+)
--- pkg/errcode/errcode.go
+++ pkg/errcode/errcode.go
@@ -1,1 +1,76 @@
 package errcode
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Error struct {
+	code    int      `json:"code"`
+	msg     string   `json:"msg"`
+	details []string `json:"details"`
+}
+
+var codes = map[int]string{}
+
+func NewError(code int, msg string) *Error {
+	if _, ok := codes[code]; ok {
+		panic(fmt.Sprintf("code %d already exists", code))
+	}
+	codes[code] = msg
+	return &Error{code: code, msg: msg}
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("code: %d, msg: %s, details: %s", e.code, e.msg, e.details)
+}
+
+func (e *Error) Code() int {
+	return e.code
+}
+
+func (e *Error) Msg() string {
+	return e.msg
+}
+
+func (e *Error) MSgf(args []interface{}) string {
+	return fmt.Sprintf(e.msg, args...)
+}
+
+func (e *Error) Details() []string {
+	return e.details
+}
+
+func (e *Error) WithDetails(details ...string) *Error {
+	newErr := *e
+	newErr.details = []string{}
+	for _, d := range details {
+		newErr.details = append(newErr.details, d)
+	}
+	return &newErr
+}
+
+func (e *Error) StatusCode() int {
+	switch e.code {
+	case Success.Code():
+		return http.StatusOK
+	case ServerError.Code():
+		return http.StatusInternalServerError
+	case InvalidParams.Code():
+		return http.StatusBadRequest
+	case UnauthorizedAuthNotExist.Code():
+		return http.StatusUnauthorized
+	case UnauthorizedAuthTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenGenerate.Code():
+		fallthrough
+	case UnauthorizedTokenTimeout.Code():
+		return http.StatusUnauthorized
+	case TooManyRequests.Code():
+		return http.StatusTooManyRequests
+
+	}
+	return http.StatusInternalServerError
+}

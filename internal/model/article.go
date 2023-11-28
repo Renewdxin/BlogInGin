ce7352e7ae6e6a16ea8ede7eb6e918b874f6c8b7
Renewdxin <cissyrenxin@icloud.com>
2023-11-28 21:29:11 +0800

    业务接口校验

--- internal/model/article.go
+++ internal/model/article.go
@@ -1,5 +1,7 @@
 package model
 
+import "BloginGin/pkg/app"
+
 // define Article
 type Article struct {
 	*Model
@@ -8,6 +10,38 @@ type Article struct {
 	Content       string `json:"content"`
 	CoverImageUrl string `json:"cover_image_url"`
 	State         uint8  `json:"state"`
+}
+
+type CountArticleRequest struct {
+	Name  string `form:"name" binding:"max=100"`
+	State uint8  `form:"state, default=1" binding:"oneof=0 1"`
+}
+
+type ArticleListRequest struct {
+	Name  string `form:"name" binding:"max=100"`
+	State uint8  `form:"state, default=1" binding:"oneof=0 1"`
+}
+
+type CreateArticleRequest struct {
+	Name      string `form:"name" binding:"required,min=3,max=100"`
+	State     uint8  `form:"state, default=1" binding:"oneof=0 1"`
+	CreatedBy string `form:"created_by" binding:"required,min=3,max=100"`
+}
+
+type UpdateArticleRequest struct {
+	ID         uint32 `form:"id" binding:"required,gte=1"`
+	Name       string `form:"name" binding:"min=3,max=100"`
+	State      uint8  `form:"state, default=1" binding:"oneof=0 1"`
+	ModifiedBy string `form:"modified_by" binding:"required,min=3,max=100"`
+}
+
+type DeleteArticleRequest struct {
+	ID uint32 `form:"id" binding:"required,gte=1"`
+}
+
+type ArticleSwagger struct {
+	List  []*Article
+	Pager *app.Pager
 }
 
 func (A Article) TableName() string {
--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -5,6 +5,33 @@ import "github.com/gin-gonic/gin"
 type Tag struct {
 }
 
+type CountTagRequest struct {
+	Name  string `form:"name" binding:"max=100"`
+	State uint8  `form:"state, default=1" binding:"oneof=0 1"`
+}
+
+type TagListRequest struct {
+	Name  string `form:"name" binding:"max=100"`
+	State uint8  `form:"state, default=1" binding:"oneof=0 1"`
+}
+
+type CreateTagRequest struct {
+	Name      string `form:"name" binding:"required,min=3,max=100"`
+	State     uint8  `form:"state, default=1" binding:"oneof=0 1"`
+	CreatedBy string `form:"created_by" binding:"required,min=3,max=100"`
+}
+
+type UpdateTagRequest struct {
+	ID         uint32 `form:"id" binding:"required,gte=1"`
+	Name       string `form:"name" binding:"min=3,max=100"`
+	State      uint8  `form:"state, default=1" binding:"oneof=0 1"`
+	ModifiedBy string `form:"modified_by" binding:"required,min=3,max=100"`
+}
+
+type DeleteTagRequest struct {
+	ID uint32 `form:"id" binding:"required,gte=1"`
+}
+
 func NewTag() Tag {
 	return Tag{}
 }
@@ -13,10 +40,28 @@ func (t Tag) Get(c *gin.Context) {
 
 }
 
+// List @Summary
+// @Produce json
+// @Param name query string false "tag name" maxlength(100)
+// @Param state query int false "state" Enums(0, 1) default(1)
+// @Param page query int false "page"
+// @Param page_size query int false "pageSize"
+// @Success 200 {object} model.TagSwagger "success"
+// @Failure 400 {object} errcode.Error
+// @Router /api/v1/tags [get]
 func (t Tag) List(c *gin.Context) {
 
 }
 
+// Create @Summary Create tags
+// @Produce json
+// @Param name body string true "tag body" minlength(3) maxlength(100)
+// @Param state body int false "state" Enums(0, 1) default(1)
+// @Param created_by body string true "founder" minlength(3) maxlength(100)
+// @Success 200 {object} model.Tag "success"
+// @Failure 400 {object} errcode.Error "request error"
+// @Failure 500 {object} errcode.Error "Internal Server Error"
+// @Router /api/v1/tags [post]
 func (t Tag) Create(c *gin.Context) {
 
 }

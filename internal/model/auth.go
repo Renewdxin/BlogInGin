9b7946229126d302e8f189b42a510af9bd737754
Renewdxin <cissyrenxin@icloud.com>
2023-12-01 20:35:36 +0800

    get jwt

--- internal/model/auth.go
+++ internal/model/auth.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
 type Auth struct {
 	*Model
 	AppKey    string `json:"app_key"`
@@ -8,4 +13,15 @@ type Auth struct {
 
 func (auth Auth) TableName() string {
 	return "blog_auth"
+}
+
+func (auth Auth) Get(db *gorm.DB) (Auth, error) {
+	var a Auth
+	db = db.Where("app_key = ? AND app_secret = ? AND is_del = ?", auth.AppKey, auth.AppSecret, 0)
+	err := db.First(&a).Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return a, err
+	}
+
+	return a, nil
 }
